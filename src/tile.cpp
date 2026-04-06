8100c119ec7e5192debe076f4017f07760d41148
LemoCoffee <jackniems@gmail.com>
2026-04-06 11:14:01 -0700

    Fixed basic tile flags

--- src/tile.cpp
+++ src/tile.cpp
@@ -1,4 +1,4 @@
 #include "tile.h"
 
-const Tile Tile::mine = Tile('m', 0x1);
-const Tile Tile::unknown = Tile(' ', 0x2);
+const Tile Tile::mine = Tile('m', 0b10);
+const Tile Tile::unknown = Tile(' ', 0b01);
