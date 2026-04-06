4956752caf7339386e523a641a9121fdd19cef05
LemoCoffee <jackniems@gmail.com>
2026-04-06 15:07:57 -0700

    Extra tests

--- src/tests/basic_game.cpp
+++ src/tests/basic_game.cpp
@@ -81,23 +81,19 @@ TEST(BASIC_TILES, 2X2_1_BOMB)
     EXPECT_FALSE(is_valid_tile(1, 1, b));
 }
 
-TEST(BASIC_TILES, TILE_SCORING)
+TEST(BASIC_TILES, NEIGHBOR_FLAG_COMPARISON)
 {
     Board b = Board(3, 3, 1);
 
     b[1][1] = Tile('1');
     EXPECT_EQ(b.neighboring_flagged_as(1, 1, Tile::unknown_flag), 8);
-    EXPECT_EQ(b.tile_possibility_score(1, 1), 8);
 
     b[2][1] = Tile('1');
     EXPECT_EQ(b.neighboring_flagged_as(1, 1, Tile::unknown_flag), 7);
     EXPECT_EQ(b.neighboring_flagged_as(2, 1, Tile::unknown_flag), 4);
-    EXPECT_EQ(b.tile_possibility_score(1, 1), 7);
-    EXPECT_EQ(b.tile_possibility_score(2, 1), 4);
 
     b[2][1] = Tile('2');
     EXPECT_EQ(b.neighboring_flagged_as(2, 1, Tile::unknown_flag), 4);
-    EXPECT_EQ(b.tile_possibility_score(2, 1), 6);
 
     b[1][0] = Tile::mine;
     EXPECT_EQ(b.neighboring_flagged_as(1, 1, Tile::unknown_flag), 6);
@@ -105,7 +101,42 @@ TEST(BASIC_TILES, TILE_SCORING)
 
     EXPECT_EQ(b.neighboring_flagged_as(1, 1, Tile::mine_flag), 1);
     EXPECT_EQ(b.neighboring_flagged_as(2, 1, Tile::mine_flag), 1);
+}
 
+TEST(BASIC_TILES, TILE_SCORING)
+{
+    Board b = Board(3, 3, 1);
+
+    b[1][1] = Tile('1');
+    EXPECT_EQ(b.tile_possibility_score(1, 1), 8);
+
+    b[2][1] = Tile('1');
+    EXPECT_EQ(b.tile_possibility_score(1, 1), 7);
+    EXPECT_EQ(b.tile_possibility_score(2, 1), 4);
+
+    b[2][1] = Tile('2');
+    EXPECT_EQ(b.tile_possibility_score(2, 1), 6);
+
+    b[1][0] = Tile::mine;
     EXPECT_EQ(b.tile_possibility_score(1, 1), 0);
     EXPECT_EQ(b.tile_possibility_score(2, 1), 3);
+}
+
+TEST(BASIC_TILES, CORNER_TILE_SCORING)
+{
+    Board b = Board(2, 2, 1);
+
+    b[1][1] = Tile('1');
+    EXPECT_EQ(b.tile_possibility_score(1, 1), 3);
+
+    b[0][1] = Tile('1');
+    EXPECT_EQ(b.tile_possibility_score(1, 1), 2);
+    EXPECT_EQ(b.tile_possibility_score(0, 1), 2);
+
+    b[0][1] = Tile('2');
+    EXPECT_EQ(b.tile_possibility_score(0, 1), 1);
+
+    b[1][0] = Tile::mine;
+    EXPECT_EQ(b.tile_possibility_score(1, 1), 0);
+    EXPECT_EQ(b.tile_possibility_score(0, 1), 1);
 }
