d5eedded7404fc8f0b2fef4dad2204797bee3e9d
LemoCoffee <jackniems@gmail.com>
2026-04-08 11:15:32 -0700

    Added variant interface

---
+++ src/variant.h
@@ -0,0 +1,18 @@
+#include "board.h"
+
+#ifndef VARIANT_H
+#define VARIANT_H
+
+class IVariant
+{
+public:
+    virtual std::vector<Board> solve(const Board &puzzle);
+    virtual std::vector<Board> get_possibilites(const int x, const int y, const Board &board);
+
+    virtual bool is_valid_tile(const int x, const int y, const Board &board);
+    virtual bool is_valid_board(const Board &board);
+};
+
+// I want functions to be inherited and implemented by children, but I don't want variants to be instantiated or contain any info
+
+#endif
