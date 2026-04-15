326b66e65d7958bd1f6697cf621042d3389536cd
LemoCoffee <jackniems@gmail.com>
2026-04-15 12:10:32 -0700

    Added to_string method for boards

---
+++ src/utility.h
@@ -0,0 +1,19 @@
+#include <vector>
+#include <string>
+#include "variant.h"
+
+std::string board_to_string(const Board &board)
+{
+    std::string output = "";
+
+    for (int y = 0; y < board.height(); y++)
+    {
+        for (int x = 0; x < board.width(); x++)
+        {
+            output += board[x][y].value;
+        }
+        output += "\n";
+    }
+
+    return output;
+}
