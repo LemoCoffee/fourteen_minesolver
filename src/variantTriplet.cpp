97ef7021bf90d7d8abad4abd8c19564c8f77c6f3
LemoCoffee <jackniems@gmail.com>
2026-04-20 11:17:53 -0700

    Added triplets

---
+++ src/variantTriplet.cpp
@@ -0,0 +1,36 @@
+#include "variantTriplet.h"
+
+bool VariantTriplet::is_valid_board(const Board &board) const
+{
+    for (int x = 0; x < board.width(); x++)
+    {
+        for (int y = 0; y < board.height(); y++)
+        {
+            if (x < board.width() - 2) {
+                if (board[x][y].is_mine() && board[x + 1][y].is_mine() && board[x + 2][y].is_mine()) {
+                    return false;
+                }
+            }
+
+            if (y < board.height() - 2) {
+                if (board[x][y].is_mine() && board[x][y + 1].is_mine() && board[x][y + 1].is_mine()) {
+                    return false;
+                }
+
+                if (x >= 2) {
+                    if (board[x][y].is_mine() && board[x - 1][y + 1].is_mine() && board[x - 2][y + 1].is_mine()) {
+                        return false;
+                    }
+                }
+
+                if (x < board.width() - 2) {
+                    if (board[x][y].is_mine() && board[x + 1][y + 1].is_mine() && board[x + 2][y + 1].is_mine()) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return VariantBasic::is_valid_board(board);
+}
